6d798aaa954fc56988ad68e70d78a0b491c7f8b8
nanperez <perez.castro.nancy@gmail.com>
2021-03-23 18:14:47 -0600

    Ejercicios en asesorias

---
+++ pilaEstatica.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define TAM 100
+
+typedef struct pila
+{
+  int tope;
+  int elementos[TAM];
+}Pila;
+
+// Funciones básicas
+void insertar(Pila *p, int valor); // push()
+int eliminar(Pila *p);         // pop()
+// Funciones auxiliares
+int vacia(Pila *p);
+void imprimir(Pila *p);
+
+int main(){
+    // Declarando e inicializando una variable de tipo Pila
+    Pila pila1={-1,{0}};
+
+
+}
+
+void insertar(Pila *p, int valor){
+    p->tope++;
+    p->elementos[p->tope]=valor;
+
+}
+
+void imprimir(Pila *p){
+    for (int i = p->tope; i >=0; i--)
+    {
+        printf("%d",p->elementos[i]);
+        
+    }
+    
+
+}
